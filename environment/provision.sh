beddcb1198f3a4f8cb5ea8438b9e14004de90717
Miles Eastwood <MEastwood@spartaglobal.com>
2019-10-13 11:38:01 +0100

    Testing Jenkins webhook

--- environment/provision.sh
+++ environment/provision.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-sudo apt-get update -y
+sudo apt-get update && sudo apt-get -y upgrade
 
 
-
+# Installing nginx
 sudo apt-get install nginx -y
 sudo systemctl start nginx
-
-
 
 # installing java
 sudo apt update
 sudo apt install openjdk-8-jdk -y
 
-
+# installing nodejs
+sudo apt-get install curl -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs -y
 
 # installing jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
@@ -21,10 +22,11 @@ sudo apt update
 sudo apt install jenkins -y
 systemctl status jenkins
 
-
-
 #opening port
 sudo ufw allow OpenSSH
 sudo ufw enable
 sudo ufw allow 8080
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+# Installing ngrok
+sudo npm i -g ngrok --unsafe-perm=true --allow-root
